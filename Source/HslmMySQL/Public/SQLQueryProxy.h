319525af5a165092b90b62c500cfca0e0bb9e1ae
HuiSeLiMing <1055907692@qq.com>
2021-04-02 12:36:31 +0800

    Blueprint Node Add

---
+++ Source/HslmMySQL/Public/SQLQueryProxy.h
@@ -0,0 +1,16 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "UObject/Object.h"
+#include "SQLQueryProxy.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class HSLMMYSQL_API USQLQueryProxy : public UObject
+{
+	GENERATED_BODY()
+};
